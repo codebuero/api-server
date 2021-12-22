ad8b1fee5393d88199104b0ee35fbe2167289418
Andreas Rotter <andreas.rotter@wunderdog.fi>
2021-12-22 12:51:02 +0100

    make api readonly

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({
+    bodyParser: false,
+    readOnly: true
+})
 
 const PORT = process.env.PORT || 5000
 
